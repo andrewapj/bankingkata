39ab3adf779e50eb5690c114beed7a12fd29a8ff
Andrew Johnson <andrew.johnson@gmail.com>
2018-11-18 22:33:38 +0000

    Initial import of the Banking Kata application with a failing unit test.

---
+++ src/main/java/com/github/andrewapj/Account.java
@@ -0,0 +1,19 @@
+package com.github.andrewapj;
+
+public class Account {
+
+    void deposit(final int amount)
+    {
+
+    }
+
+    void withdraw(final int amount)
+    {
+
+    }
+
+    String printStatement()
+    {
+    return "";
+    }
+}
---
+++ src/test/java/com/github/andrewapj/AccountTest.java
@@ -0,0 +1,15 @@
+package com.github.andrewapj;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    public void test()
+    {
+        fail("a failing test");
+    }
+
+}
